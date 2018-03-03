91f7f19628e0cc8e350628766e3777d8c00cd95e
Jeff Jones <jeff@beartooth.com>
2018-03-02 20:09:16 -0800

    Initial Commit

---
+++ gyro-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyro-demo
+//
+//  Created by Jefferson Jones on 3/2/18.
+//  Copyright © 2018 Jefferson Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
